fa10a1492f014850b04eed68ff2a0355b9fdbd9a
Fromont Boissel <pierrefromont@outlook.fr>
2023-05-31 11:09:20 +0200

    begin CheckBoxPanelStyle

--- src/trellolite/style/CheckBoxPanelStyle.java
+++ src/trellolite/style/CheckBoxPanelStyle.java
@@ -1,11 +1,41 @@
 package trellolite.style;
 
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
 
-public class CheckBoxPanelStyle extends PanelStyle{
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class CheckBoxPanelStyle extends PanelStyle {
 
     public CheckBoxPanelStyle(ArrayList<String> fields) {
         super(new FlowLayout(FlowLayout.CENTER));
+        for (String item : fields) {
+            JCheckBox checkBox = new JCheckBox(item);
+            checkBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+            add(checkBox);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                ArrayList<String> fields = new ArrayList<String>();
+                fields.add("Field 1");
+                fields.add("Field 2");
+                fields.add("Field 3");
+                fields.add("Field 4");
+                fields.add("Field 5");
+                CheckBoxPanelStyle panel = new CheckBoxPanelStyle(fields);
+                JFrame frame = new JFrame("CheckBoxPanelStyle");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(panel);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+
     }
 }
